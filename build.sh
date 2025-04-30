3997dcaa1785f3310b49bf1755f20b4d872af86e
YuzakiKokuban <heibanbaize@gmail.com>
2025-04-30 22:36:45 +0800

    升级到DYD9

--- build.sh
+++ build.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
+TOOLCHAIN=$(realpath "/home/kokuban/PlentyofToolchain/toolchainS23/prebuilts")
 
-#
-# Tested on Ubuntu 22.04
-#
-# 1. Install Clang/LLVM
-#
-# apt install -y clang llvm lld
-#
-# Or get Clang/LLVM from https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r450784e.tar.gz
-# and add bin dir to PATH
-#
-# 2. Install required packages
-# apt install -y git zip build-essential xz-utils bison flex libz-dev libssl-dev libelf-dev bc cpio python3 pahole
-#
-# 3. Run build.sh to build kernel, do a clean before build is recommended
-# make clean && rm -rf ./out
-# ./build.sh <your defconfig>
-#
-# 4. Image output to out/arch/arm64/boot/Image
-# AnyKernel3 zip output to out/AnyKernel3/<your defconfig>_kernel_*.zip
-export PATH="/home/kokuban/toolchain/prebuilts/clang/host/linux-x86/clang-r450784e/bin:usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin:$PATH"
+export PATH=$TOOLCHAIN/build-tools/linux-x86/bin:$PATH
+export PATH=$TOOLCHAIN/build-tools/path/linux-x86:$PATH
+export PATH=$TOOLCHAIN/clang/host/linux-x86/clang-r450784e/bin:$PATH
+export PATH=$TOOLCHAIN/clang-tools/linux-x86/bin:$PATH
+export PATH=$TOOLCHAIN/kernel-build-tools/linux-x86/bin:$PATH
 
 echo $PATH
 
@@ -31,7 +17,7 @@ TARGET_DEFCONFIG=${1:-kalama_gki_defconfig}
 
 cd "$(dirname "$0")"
 
-LOCALVERSION=-android13-Kokuban-Firefly-CYC1-LKM
+LOCALVERSION=-android13-Kokuban-Firefly-DYD9-LKM
 
 ARGS="
 CC=clang
