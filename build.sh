967b619d3c634eff6461ffa9308956f7c858eeb8
YuzakiKokuban <heibanbaize@gmail.com>
2025-05-01 05:45:18 +0800

    修改：     build.sh

--- build.sh
+++ build.sh
@@ -35,8 +35,18 @@ make -j$(nproc) -C $(pwd) O=$(pwd)/out ${ARGS} $TARGET_DEFCONFIG
   -d SECURITY_DEFEX \
   -d INTEGRITY \
   -d FIVE \
-  -d TRIM_UNUSED_KSYMS
-
+  -d TRIM_UNUSED_KSYMS \
+  -d PROCA \
+  -d PROCA_GKI_10 \
+  -d PROCA_S_OS \
+  -d PROCA_CERTIFICATES_XATTR \
+  -d PROCA_CERT_ENG \
+  -d PROCA_CERT_USER \
+  -d GAF_V6 \
+  -d FIVE \
+  -d FIVE_CERT_USER \
+  -d FIVE_DEFAULT_HASH
+  
 if [ "$LTO" = "thin" ]; then
   ./scripts/config --file out/.config -e LTO_CLANG_THIN -d LTO_CLANG_FULL
 fi
@@ -48,8 +58,14 @@ cd out
 if [ ! -d AnyKernel3 ]; then
   git clone --depth=1 https://github.com/YuzakiKokuban/AnyKernel3.git -b kalama
 fi
-cp arch/arm64/boot/Image AnyKernel3/zImage
+cp arch/arm64/boot/Image AnyKernel3/Image
 cd AnyKernel3
+chmod +x patch_linux
+./patch_linux
+mv oImage zImage
+rm -f oImage
+rm -f Image
+rm -f patch_linux
 cd ..
 name=S23_kernel_`cat include/config/kernel.release`_`date '+%Y_%m_%d'`
 cd AnyKernel3
