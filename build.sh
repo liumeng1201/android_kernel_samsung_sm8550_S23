794c890d7e5ae4e9807448e1c973bdc5892a603b
YuzakiKokuban <heibanbaize@gmail.com>
2025-05-18 13:07:59 +0800

    修改：     build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ TARGET_DEFCONFIG=${1:-kalama_gki_defconfig}
 
 cd "$(dirname "$0")"
 
-LOCALVERSION=-android13-Kokuban-Firefly-DYD9-SukiSUU
+LOCALVERSION=-android13-Kokuban-Firefly-DYDA-SukiSUU
 
 ARGS="
 CC=clang
